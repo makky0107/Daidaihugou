dbac02b15a19109f120fdc6938fb973329b2b354
makky0107 <122294224+makky0107@users.noreply.github.com>
2023-12-15 20:19:22 +0900

    1

--- Assets/scriptScroll/LoggerScroll.cs
+++ Assets/scriptScroll/LoggerScroll.cs
@@ -21,7 +21,7 @@ public class LoggerScroll : MonoBehaviourPunCallbacks
     [PunRPC]
     public void AddLog(string logText)
     {
-        Debug.LogWarning($"<size=24><color=blue>AddLog</color></size>");
+        //Debug.LogWarning($"<size=24><color=blue>AddLog</color></size>");
         //delta.y += 40;
         //this.gameObject.transform.localPosition = delta;
         log.text += "\n" + logText;
